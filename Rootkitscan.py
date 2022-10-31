d52e410c31a02d93421445ec5296b080d0894508
root.flow <99812933+root-flow@users.noreply.github.com>
2022-10-31 21:21:35 +0300

    Create Rootkitscan.py

---
+++ Rootkitscan.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+
+import os
+
+
+
+os.system("apt-get install figlet")
+
+os.system("clear")
+
+os.system("figlet BHECY")
+
+os.system("figlet ROOTKIT TARAMA")
+
+
+
+print("""
+
+
+
+RootKit Tarama Aracına Hoş Geldin...
+
+
+
+""")
+
+
+
+os.system("chkrootkit")
